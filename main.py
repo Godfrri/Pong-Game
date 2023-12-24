ddfc5206d8956b66de0fc2ef10dccaca5e0348ff
Godfrri <nixongodfrricroos@gmail.com>
2023-12-24 12:41:53 +0530

    paddles are added

--- main.py
+++ main.py
@@ -24,17 +24,45 @@ class PongBall(Widget):
 
 
 
+class PongPaddle(Widget):
+    pass
+
 
 # move the ball by calling the move function and other stuff
+
+# on_touch_down() - When our fingers/mouse touches the screen
+# on_touch_up() - When we lift our finger off the screen after touching it
+# on_touch_move() - When we drag our finger on the screen
+
 class PongGame(Widget):
 
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
     def serve_ball(self):
         self.ball.velocity = Vector (4, 0).rotate(randint(0, 360))
 
     def update(self, dt):
         self.ball.move ()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.y > self.height - 50):
+            self.ball.velocity_y *= -1
+        
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.x > self.width - 50):
+            self.ball.velocity_x *= -1
+    
+    def on_touch_move(self, touch):
+        if touch.x < self.width * 1/4:
+            self.player1.center_y = touch.y
+
+        if touch.x > self.width * 3/4:
+            self.player2.center_y = touch.y
+
+
+        return super().on_touch_move(touch)
 
 
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 
 #! https://youtube.com/playlist?list=PLhTjy8cBISEpobkPwLm71p5YNBzPH9m9V&si=UyztcDxR5HKPgqV_
-#! Use Python 3.8.5
+#! Use Python 3.9.6
 
 #! Add all dependencies here
 os.system('pip install kivy[base]==2.2.1')
