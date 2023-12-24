db78b9ea094473ed31a75a9a4309937d9a155c90
Godfrri <nixongodfrricroos@gmail.com>
2023-12-24 11:39:23 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+
+#! https://youtube.com/playlist?list=PLhTjy8cBISEpobkPwLm71p5YNBzPH9m9V&si=UyztcDxR5HKPgqV_
+#! Use Python 3.8.5
+
+#! Add all dependencies here
+os.system('pip install kivy[base]==2.2.1')
+os.system('pip install kivymd==1.1.1')
+
+
+
+print('All dependencies installed')
+
